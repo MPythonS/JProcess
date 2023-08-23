270575b9237047799616f19ff357e4d8e1c5e472
MPythonS <starkus.mantas@gmail.com>
2023-08-23 20:53:19 +0300

    Pirmas

---
+++ JuicePro/j_types/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from .models import JuiceTypes, JuiceFruits, JuiceVegetables, JuiceBerries
+# Register your models here.
+class JuiceTypesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'active',
+        'created',
+        'updated',
+    )
+    list_filter = ('active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class JuiceFruitsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'active',
+        'created',
+        'updated',
+    )
+    list_filter = ('active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class JuiceVegetablesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'active',
+        'created',
+        'updated',
+    )
+    list_filter = ('active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class JuiceBerriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'active',
+        'created',
+        'updated',
+    )
+    list_filter = ('active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class NewAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'active',
+        'created',
+        'updated',
+    )
+    list_filter = ('active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+admin.site.register(JuiceTypes, JuiceTypesAdmin)
+admin.site.register(JuiceFruits, JuiceFruitsAdmin)
+admin.site.register(JuiceVegetables, JuiceVegetablesAdmin)
+admin.site.register(JuiceBerries, JuiceBerriesAdmin)
